94aedcf3c45a8a2a3bd1bc04104c37a39f6ff90f
ducoquelicot <fmeijer@stanford.edu>
2019-04-01 22:41:37 -0700

    created search page

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,9 @@ from app import observ
 
 @observ.route('/')
 @observ.route('/index')
-
 def index():
     return render_template('index.html')
+
+@observ.route('/search')
+def search():
+    return render_template('search.html', title='search')
