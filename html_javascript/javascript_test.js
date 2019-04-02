8650d318ee36acc39ec590511d539ca102916d72
ducoquelicot <fmeijer@stanford.edu>
2019-04-01 17:36:37 -0700

    created static html index page

---
+++ html_javascript/javascript_test.js
@@ -0,0 +1,71 @@
+// This is a single line comment
+// alert("This is an alert box!");
+/* This is a multi line comment
+As you can see it has multiple lines */
+
+// var x = 100;
+// document.write(x);
+
+// var x = 10
+// document.write(++x)
+
+// var string = 'I am learning JavaScript ';
+// var string_2 = 'and it involves a lot of typing.';
+// document.write(string + string_2)
+
+// var a = 11, b = 10
+// if (a < b) {
+//     document.write('Seven is indeed smaller than ten.')
+// }
+// else {
+//     document.write('These numbers don\'t add up.')
+// }
+
+// var a = 11, b = 10
+// a > b ? alert('a') : alert('b')
+
+// var day = 7
+// switch(day) {
+//     case 1:
+//     document.write('Monday')
+//     break
+//     case 2:
+//     document.write('Tuesday')
+//     break
+//     case 7: 
+//     document.write('Sunday')
+//     break
+//     default:
+//     document.write('Another day')
+// }
+
+// for (i=1; i<=5; i++) {
+//     document.write(i + "<br/>")
+// }
+
+// var i = 1
+// for (; i<10 ;) {
+//     document.write(i + "<br/>")
+//     i++
+// }
+
+// var i = 20
+// do {
+//     document.write(i + "<br/>")
+//     i++
+// }
+// while (i<=25)
+
+// function person(name, age, color) {
+//     this.name = name
+//     this.age = age
+//     this.color = color
+//     this.change_color = function(color) {
+//         this.color = color
+//     }
+// }
+
+// var person_1 = new person("John", 42, "green")
+// person_1.change_color('blue')
+// document.write(person_1.color)
+
