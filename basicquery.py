ed033c3dea1befd6f86a5df6f3f3007ff8c77ca6
ducoquelicot <fmeijer@stanford.edu>
2019-04-19 01:29:46 -0700

    basic query uploaded 4/19

--- basicquery.py
+++ basicquery.py
@@ -4,23 +4,15 @@ from elasticsearch_dsl import Search, Q
 es = Elasticsearch()
 q = Q("bool",
       must = [
-        Q("match", type = "agenda"),
-        Q("bool",
-          minimum_should_match = 1,
-          should = [
-            Q("match", city = "paloalto"),
-            Q("match", city = "redwoodcity")
-            ]
-        ),
-        Q("bool",
-          minimum_should_match = 1,
-          should = [
-            Q("match", body = "Members"),
-            Q("match", body = "Comment")
-            ]
+        Q("match",
+          body = {
+            "minimum_should_match" : 1,
+            "query" : "President whistle",
+            "operator" : "or"
+            }
         )
-      ]
-    )
+    ]
+)
 
 s = Search(using=es, index="paloalto").query(q)
 total = s.count()
